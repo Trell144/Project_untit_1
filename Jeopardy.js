4395a01a9cf993a7a2b51e451329c43c429e1ba9
Catrell Matthews <catrellmatthewss@gmail.com>
2018-11-21 17:37:36 -0500

    more styling & working on score

--- Jeopardy.js
+++ Jeopardy.js
@@ -1,36 +1,8 @@
    alert('WELCOME TO JEOPARDY! 1st Player to reach 2,000 WINS! Click \"OK" to play!' + "\t• all answers should be in lower case\n" 
    + '\t• answers should be phrased like a question\n');
-//   
 
-//     var playerOne= 0;
 
-// const gameData = {
-//     questions : {
-//         questionsOptions: ["What is a common K9 intestinal parasite?"],
-//         answer: "Round Worms"        
-//     }
-// }
 
-//     var playerInput = prompt(gameData.questions.questionsOptions);
-
-//     if (playerInput == gameData.questions.answer){
-// console.log("correct answer");
-//     }
-
-//     if (playerInput != gameData.questions.questionsOptions){
-//         console.log('wrong answer, next players turn')
-//     }
-
-// let question = ''
-// let answer = ''
-
-// let questionObject = {
-//     question: 'What the most common K9 intestinal parisite?',
-//     answer: 'worms'
-// 
-//}
-
-var gameScore = 0;
 
 
 let questionsArray = [{
